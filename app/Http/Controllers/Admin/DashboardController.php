08c95186a3ad0dc6bfd6a7aec914cec02a81bbe4
chrialge <chrialge@gmail.com>
2024-05-20 16:36:19 +0200

    add fix problem with route

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-class dashboardController extends Controller
+class DashboardController extends Controller
 {
     function index()
     {
